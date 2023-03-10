75ec5857a596ad4e8bbcae7a801c14e6dd015b66
tfu2003 <officialtonyfu@gmail.com>
2023-03-10 00:29:06 -0800

    made bet.js take reactions for user

--- commands/bet.js
+++ commands/bet.js
@@ -1,17 +1,43 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('bet')
-		.setDescription('Bet on things.')
-		.addNumberOption(option => option.setName('amount').setDescription('Over what number?')),
-	async execute(interaction) {
-		const amount = interaction.options.getNumber('amount');
-		const message = await interaction.reply({ content: `Over ${amount}?`, fetchReply: true });
-		message.react('⬆️')
-			.then(() => message.react('⬇️'))
-			.catch(() => console.error('One of the emojis failed to react.'));
-        return message;
-	},
+  data: new SlashCommandBuilder()
+    .setName("bet")
+    .setDescription("Bet on things.")
+    .addNumberOption((option) =>
+      option.setName("amount").setDescription("Over what number?")
+    ),
+  async execute(interaction) {
+    const amount = interaction.options.getNumber("amount");
+    const message = await interaction.reply({
+      content: `Over ${amount}?`,
+      fetchReply: true,
+    });
 
+    message.react("⬆️").then(() => message.react("⬇️"));
+
+    const filter = (reaction, user) => {
+      return (
+        ["⬆️", "⬇️"].includes(reaction.emoji.name) &&
+        user.id === interaction.user.id
+      );
+    };
+
+    message
+      .awaitReactions({ filter, max: 1, time: 10000, errors: ["time"] })
+      .then((collected) => {
+        const reaction = collected.first();
+
+        if (reaction.emoji.name === "⬆️") {
+          message.reply("You reacted with a thumbs up.");
+        } else {
+          message.reply("You reacted with a thumbs down.");
+        }
+      })
+      .catch((collected) => {
+        message.reply(
+          "You reacted with neither a thumbs up, nor a thumbs down."
+        );
+      });
+  },
 };
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions] });
 
 client.commands = new Collection();
 const commandsPath = path.join(__dirname, 'commands');
