f9808f28992a4dd870686bed532abb2cf8d028ae
tfu2003 <officialtonyfu@gmail.com>
2023-03-10 17:16:05 -0800

    made ping work and made it not affect me

--- commands/ping.js
+++ commands/ping.js
@@ -1,19 +1,32 @@
-const { SlashCommandBuilder } = require('discord.js');
-const dotenv = require('dotenv');
+const { SlashCommandBuilder } = require("discord.js");
+const dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('ping')
-		.setDescription('A perfectly normal command that you should try!'),
-	async execute(interaction) {
-		const name = "<@182973560719933441>" 
-		await interaction.reply({ content: `${name}`})
-		for (let i = 0; i < 20; i++) {
-			await interaction.followUp({ content: `${name}`})
-		}		  
-	},
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("A perfectly normal command that you should try!")
+    .addStringOption((option) =>
+      option.setName("id").setDescription("If you use my ID I will boom you")
+    ),
+  async execute(interaction) {
+    const id = interaction.options.getString("id");
+    var user = `<@${interaction.user.id}>`;
+    if (id.includes("<@266325246863343616>")) {
+      try {
+        await interaction.reply(`MAN SHUT YO BITCH ASS UP ${user}`);
+      } catch (e) {
+        console.error(e);
+      }
+    } else {
+      try {
+        await interaction.reply({ content: `${id}` });
+        for (let i = 0; i < 20; i++) {
+          await interaction.followUp({ content: `${id}` });
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  },
 };
-
-
-  
