33fc205d5a599a8ed251095940466748c2fab3f4
tfu2003 <officialtonyfu@gmail.com>
2023-03-11 00:57:28 -0800

    made it so only people with roles can use ping

--- commands/ping.js
+++ commands/ping.js
@@ -10,23 +10,31 @@ module.exports = {
       option.setName("id").setDescription("If you use my ID I will boom you")
     ),
   async execute(interaction) {
+    const hasRole = interaction.member.roles.cache.some(
+      (r) => r.name === "human"
+    );
+    console.log(hasRole);
     const id = interaction.options.getString("id");
     var user = `<@${interaction.user.id}>`;
-    if (id.includes("<@266325246863343616>")) {
-      try {
-        await interaction.reply(`MAN SHUT YO BITCH ASS UP ${user}`);
-      } catch (e) {
-        console.error(e);
+    if (hasRole) {
+      if (id.includes("<@266325246863343616>")) {
+        try {
+          await interaction.reply(`MAN SHUT YO BITCH ASS UP ${user}`);
+        } catch (e) {
+          console.error(e);
+        }
+      } else {
+        try {
+          await interaction.reply({ content: `${id}` });
+          for (let i = 0; i < 19; i++) {
+            await interaction.followUp({ content: `${id}` });
+          }
+        } catch (e) {
+          console.error(e);
+        }
       }
     } else {
-      try {
-        await interaction.reply({ content: `${id}` });
-        for (let i = 0; i < 19; i++) {
-          await interaction.followUp({ content: `${id}` });
-        }
-      } catch (e) {
-        console.error(e);
-      }
+      await interaction.reply("You are developmentally delayed sir.");
     }
   },
 };
