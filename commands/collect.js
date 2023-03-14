6bea5fbf61fca49760d1314ad007d6ed45fa4578
tfu2003 <officialtonyfu@gmail.com>
2023-03-14 01:01:11 -0700

    added a cooldown for collect command

--- commands/collect.js
+++ commands/collect.js
@@ -1,30 +1,39 @@
 const { SlashCommandBuilder } = require("discord.js");
 const profileModel = require("../models/profileSchema");
+const usedCommand = new Set();
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("collect")
     .setDescription("Use to get your daily points"),
   async execute(interaction) {
-    try {
-      profileData = await profileModel.findOne({ userID: interaction.user.id });
-      newPoints = profileData.points + 1000;
-      if (profileData) {
-        await profileModel.updateOne(
-          { userID: interaction.user.id },
-          { $set: { points: newPoints } }
-        );
-        await interaction.reply({
-          content: `You currently now have ${newPoints} points.`,
-        });
-      } else {
-        await interaction.reply({
-          content:
-            "You do not currently have a profile. Do /create to make a profile.",
-        });
+    if (usedCommand.has(interaction.user.id)) {
+      await interaction.reply({ content: `Wait 1 hour to get more points <@${interaction.user.id}>.`})
+    } else {
+      try {
+        profileData = await profileModel.findOne({ userID: interaction.user.id });
+        newPoints = profileData.points + 5000;
+        if (profileData) {
+          usedCommand.add(interaction.user.id);
+          await profileModel.updateOne(
+            { userID: interaction.user.id },
+            { $set: { points: newPoints } }
+          );
+          await interaction.reply({
+            content: `You now have ${newPoints} points.`,
+          });
+          setTimeout(() => {
+            usedCommand.delete(interaction.user.id);
+          }, 3600000);
+        } else {
+          await interaction.reply({
+            content:
+              "You do not currently have a profile. Do /create to make a profile.",
+          });
+        }
+      } catch (e) {
+        console.log(e);
       }
-    } catch (e) {
-      console.log(e);
     }
   },
 };
