6a3fdd6c54b8f323203cca049b682778f8071fa6
tfu2003 <officialtonyfu@gmail.com>
2023-03-14 11:52:06 -0700

    added leaderboard command

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,4 @@
 const { SlashCommandBuilder } = require("discord.js");
-const dotenv = require("dotenv");
-dotenv.config();
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -9,34 +7,6 @@ module.exports = {
     .addStringOption((option) =>
       option.setName("id").setDescription("If you ping me, I will boom you.")
     ),
-  // async execute(interaction) {
-  //   const hasRole = interaction.member.roles.cache.some(
-  //     (r) => r.name === "human"
-  //   );
-  //   const id = interaction.options.getString("id");
-  //   var user = `<@${interaction.user.id}>`;
-  //   if (hasRole) {
-  //     if (id.includes("<@266325246863343616>")) {
-  //       try {
-  //         await interaction.reply(`MAN SHUT YO BITCH ASS UP ${user}`);
-  //       } catch (e) {
-  //         console.error(e);
-  //       }
-  //     } else {
-  //       try {
-  //         await interaction.reply({ content: `${id}` });
-  //         for (let i = 0; i < 19; i++) {
-  //           await interaction.followUp({ content: `${id}` });
-  //         }
-  //       } catch (e) {
-  //         console.error(e);
-  //       }
-  //     }
-  //   } else {
-  //     await interaction.reply(`You are developmentally delayed ${user}`);
-  //   }
-  // },
-
   async execute(interaction) {
     const id = interaction.options.getString("id");
     var user = `<@${interaction.user.id}>`;
