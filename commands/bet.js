ca16e2b5425100f7b81de0ee6cf5dc3e1df8d92a
tfu2003 <officialtonyfu@gmail.com>
2023-03-14 00:35:40 -0700

    fixed replies for bet command

--- commands/bet.js
+++ commands/bet.js
@@ -9,6 +9,8 @@ module.exports = {
     ),
   async execute(interaction) {
     var ids = [];
+    var overs = [];
+    var unders = [];
     const amount = interaction.options.getNumber("amount");
     const message = await interaction.reply({
       content: `Over ${amount}?`,
@@ -36,17 +38,40 @@ module.exports = {
         if (user.id != 990453979202867211 && !(ids.includes(user.id))) {
           message.reply(`${user} thinks over.`);
           ids.push(user.id);
+          overs.push(`<@${user.id}>`);
         }
       } else {
         if (user.id != 990453979202867211 && !(ids.includes(user.id))) {
           message.reply(`${user} thinks under.`);
           ids.push(user.id);
+          unders.push(`<@${user.id}>`);
         }
       }
     });
 
-    collector.on("end", (collected) => {
-      message.reply("Betting time has ended.");
+    collector.on("end", async (collected) => {
+      message.reactions.removeAll();
+      await message.reply("Betting time has ended.");
+      const result = await interaction.followUp({ content: "React to give the result of the bet."});
+      result.react("⬆️").then(() => result.react("⬇️"));
+  
+      const collector = result.createReactionCollector({
+        filter,
+        max: 100,
+      });
+
+  
+      collector.on("collect", (reaction, user) => {
+        if (reaction.emoji.name === "⬆️" && user.id == interaction.user.id) {
+          message.reply(`The result was over ${amount}. Congraulations to ${overs.join(" ")}.`);
+          result.reactions.removeAll();
+        } else {
+          if (user.id == interaction.user.id) {
+          message.reply(`The result was under ${amount}. Congratulations to ${unders.join(" ")}.`);
+          result.reactions.removeAll();
+          }
+        }
+      });
     });
   },
 };
