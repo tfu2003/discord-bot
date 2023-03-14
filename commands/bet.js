a0eccddc5d9937a5509c8cd1208e37f797ff1336
tfu2003 <officialtonyfu@gmail.com>
2023-03-14 13:03:25 -0700

    added bet embed to show bettors

--- commands/bet.js
+++ commands/bet.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require("discord.js");
+const { EmbedBuilder } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -13,7 +14,7 @@ module.exports = {
     var unders = [];
     const amount = interaction.options.getNumber("amount");
     const message = await interaction.reply({
-      content: `Over ${amount}?`,
+      content: `Over ${amount} kills?`,
       fetchReply: true,
     });
 
@@ -35,13 +36,13 @@ module.exports = {
 
     collector.on("collect", (reaction, user) => {
       if (reaction.emoji.name === "⬆️") {
-        if (user.id != 990453979202867211 && !(ids.includes(user.id))) {
+        if (user.id != 990453979202867211 && !ids.includes(user.id)) {
           message.reply(`${user} thinks over.`);
           ids.push(user.id);
           overs.push(`<@${user.id}>`);
         }
       } else {
-        if (user.id != 990453979202867211 && !(ids.includes(user.id))) {
+        if (user.id != 990453979202867211 && !ids.includes(user.id)) {
           message.reply(`${user} thinks under.`);
           ids.push(user.id);
           unders.push(`<@${user.id}>`);
@@ -52,23 +53,59 @@ module.exports = {
     collector.on("end", async (collected) => {
       message.reactions.removeAll();
       await message.reply("Betting time has ended.");
-      const result = await interaction.followUp({ content: "React to give the result of the bet."});
+
+      const betEmbed = new EmbedBuilder()
+        .setColor(0x0099ff)
+        .setTitle(`Over ${amount} kills?`)
+        .addFields(
+          {
+            name: "Over",
+            value: `${overs.length !== 0 ? overs.join("\n") : "No bettors"}`,
+          },
+          {
+            name: "Under",
+            value: `${unders.length !== 0 ? unders.join("\n") : "No bettors"}`,
+          }
+        )
+        .setTimestamp();
+      await message.reply({ embeds: [betEmbed] });
+
+      const result = await interaction.followUp({
+        content: "React to give the result of the bet.",
+      });
       result.react("⬆️").then(() => result.react("⬇️"));
-  
+
       const collector = result.createReactionCollector({
         filter,
         max: 100,
       });
 
-  
       collector.on("collect", (reaction, user) => {
         if (reaction.emoji.name === "⬆️" && user.id == interaction.user.id) {
-          message.reply(`The result was over ${amount}. Congraulations to ${overs.join(" ")}.`);
-          result.reactions.removeAll();
+          if (overs.length !== 0) {
+            message.reply(
+              `The result was over ${amount}. Congraulations to ${overs.join(
+                " "
+              )}.`
+            );
+            result.delete();
+          } else {
+            message.reply(`The result was over ${amount}. Nobody won.`);
+            result.delete();
+          }
         } else {
           if (user.id == interaction.user.id) {
-          message.reply(`The result was under ${amount}. Congratulations to ${unders.join(" ")}.`);
-          result.reactions.removeAll();
+            if (unders.length !== 0) {
+              message.reply(
+                `The result was under ${amount}. Congratulations to ${unders.join(
+                  " "
+                )}.`
+              );
+              result.delete();
+            } else {
+              message.reply(`The result was under ${amount}. Nobody won.`);
+              result.delete();
+            }
           }
         }
       });
--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -1,21 +1,22 @@
-const { EmbedBuilder } = require('discord.js');
+const { EmbedBuilder } = require("discord.js");
 const { SlashCommandBuilder } = require("discord.js");
 const profileModel = require("../models/profileSchema");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-      .setName("lb")
-      .setDescription("Use to see leaderboard for most points."),
-    async execute(interaction) {
-        let leaderboard = [];
-        const sorted = await profileModel.find().sort({"points": -1}).limit(10);
-        sorted.forEach(element => leaderboard.push(`<@${element.userID}>: ${element.points}`));
-        const exampleEmbed = new EmbedBuilder()
-        .setColor(0x0099FF)
-        .setTitle('Top 10 For Most Points')
-        .setDescription(`${leaderboard.join("\n")}`)
-        .setTimestamp()
-    
-    interaction.reply({ embeds: [exampleEmbed] });
-    },
-  };
+  data: new SlashCommandBuilder()
+    .setName("lb")
+    .setDescription("Use to see leaderboard for most points."),
+  async execute(interaction) {
+    let leaderboard = [];
+    const sorted = await profileModel.find().sort({ points: -1 }).limit(10);
+    sorted.forEach((element) =>
+      leaderboard.push(`<@${element.userID}>: ${element.points}`)
+    );
+    const leaderboardEmbed = new EmbedBuilder()
+      .setColor(0x0099ff)
+      .setTitle("Top 10 For Most Points")
+      .setDescription(`${leaderboard.join("\n")}`)
+      .setTimestamp();
+    interaction.reply({ embeds: [leaderboardEmbed] });
+  },
+};
